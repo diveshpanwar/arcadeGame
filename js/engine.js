641369b334358eae44bf8d95604e5a45eee43f62
diveshpanwar <divesh.panwar@gmail.com>
2017-07-18 19:00:22 +0530

    Minor Fix 2

--- js/engine.js
+++ js/engine.js
@@ -27,7 +27,8 @@ var Engine = (function(global) {
     canvas.id = 'canvas';
     canvas.width = 505;
     canvas.height = 606;
-    doc.body.appendChild(canvas);
+    $("#gameContainer").append(canvas);
+  //  doc.body.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
